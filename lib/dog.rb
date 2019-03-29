04baae7c0b179981cb14edecf799a431b6297099
John Hayes <johnnyhayes7@gmail.com>
2019-03-29 15:50:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -59,7 +59,7 @@ class Dog
     sql = "SELECT * FROM dogs WHERE name = ?"
     dog_attrs = DB[:conn].execute(sql, name).flatten
     dog = create(id: dog_attrs[0], name: dog_attrs[1], breed: dog_attrs[2])
-    binding.pru
+    binding.pry
     dog
   end
   
